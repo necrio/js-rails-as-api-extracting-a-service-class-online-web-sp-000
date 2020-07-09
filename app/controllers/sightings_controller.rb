5a1e5504df8d52e1fcf45161037f2a72d72b2b67
Keith Isham <kisham8899@gmail.com>
2020-07-09 21:29:14 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,4 +3,8 @@ class SightingsController < ApplicationController
         @sighting = Sighting.find(params[:id])
         render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
     end
+    class Location < ApplicationRecord
+  has_many :sightings
+  has_many :birds, through: :sightings
+end
 end
